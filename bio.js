24b65519f5e01ddc5ef96c6f2a1065858d5207fe
Michal <michal.batsch@gmail.com>
2026-08-24 13:49:53 +0200

    Dodanie pliku bio.js i usunięcie polyfill

---
+++ bio.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutText = "Data scientist and researcher with 10+ years of experience in computational analysis, signal and image processing, and machine learning. Combines a strong foundation in mathematics and mechanical engineering with hands-on expertise in deep learning, computer vision, and scientific programming to solve complex, data-driven problems across inter disciplinary domains.";
+    document.querySelectorAll(".info").forEach(el => {
+        el.textContent = aboutText;
+    });
+});

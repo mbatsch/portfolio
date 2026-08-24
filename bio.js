dac32b639a53f2cd0ae9da366a88064a6b2c9000
Michal <michal.batsch@gmail.com>
2026-08-24 14:20:34 +0200

    interdisciplinaryl

--- bio.js
+++ bio.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const aboutText = "Data scientist and researcher with 10+ years of experience in computational analysis, signal and image processing, and machine learning. Combines a strong foundation in mathematics and mechanical engineering with hands-on expertise in deep learning, computer vision, and scientific programming to solve complex, data-driven problems across inter disciplinary domains.";
+    const aboutText = "Data scientist and researcher with 10+ years of experience in computational analysis, signal and image processing, and machine learning. Combines a strong foundation in mathematics and mechanical engineering with hands-on expertise in deep learning, computer vision, and scientific programming to solve complex, data-driven problems across interdisciplinary domains.";
     document.querySelectorAll(".info").forEach(el => {
         el.textContent = aboutText;
     });
